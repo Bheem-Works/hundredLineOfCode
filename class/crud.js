0e4f25b652aa7bc25bdc8c4aff1939078d073bae
bheem-works <shokokomichan098@gmail.com>
2025-07-20 13:07:32 +0545

    FlowUp

--- class/crud.js
+++ class/crud.js
@@ -16,3 +16,14 @@ class AddList {
 const Push = new AddList();
 console.log(Push.add("new"));
 console.log("another one\n\n ----");
+          
+// let's find out the even and the other's number;
+let items = [1, 2, 3, 4];
+// i have to do with the index; like this
+// [1,2,3,]
+// (1) -> gives the the 1 index value which is 2 and it will remove the all unmatching value; 
+function filterItems(items, find) {
+  const filterItem = items.filter((itm) => itm === find);
+  return filterItem;
+}
+
