74b03d475d04d7a7f6bcb79a4ea98e3e6ba6e7e8
bheem-works <shokokomichan098@gmail.com>
2025-07-21 20:18:16 +0545

    push the file

--- class/chapter2This.js
+++ class/chapter2This.js
@@ -18,25 +18,25 @@ function foo() {
 }
 
 var a = 2;
-// but this was running in the terminal; 
+// but this was running in the terminal;
 
 foo(); // 2
 
-// implicit binding; 
+// implicit binding;
 // -> implicit binding is when the function is called with an object as its context, and that object is used as `this` inside the function.
 function foo() {
   console.log(this.a);
 }
 function maan() {
   console.log(this.b);
-};
+}
 
 var obj = {
-  a:3,
-  b:"this is implicit binding",
+  a: 3,
+  b: "this is implicit binding",
   foo: foo, // this is explicit binding
-  maan:maan // this is implicit binding
-}
+  maan: maan, // this is implicit binding
+};
 
 obj.foo(); // 3
 obj.maan(); // "this is implicit binding"
@@ -48,8 +48,8 @@ function foo() {
 }
 var object = {
   a: 2,
-  foo: foo
-}
+  foo: foo,
+};
 
 var foo = object.foo; // function is now detached from the object
 // foo(); // TypeError: this.a is undefined in strict mode, or logs undefined in
@@ -59,17 +59,76 @@ foo(); // "oops i am the global a"
 function foo() {
   console.log(this.b);
 }
-function dofoo(fn){
+function dofoo(fn) {
   fn();
 }
 
 var callObj = {
   a: 2,
-  foo: foo
-}
-var b =  "oops i am the global b";
+  foo: foo,
+};
+var b = "oops i am the global b";
 dofoo(callObj.foo); // "oops i am the global a" - implicit lost binding
 
-// explicit binding; 
+// explicit binding;
 // explicit binding is when you use `.call()`, `.apply()`, or `.bind()` to set the context of `this` explicitly.
 // context is set to the first argument of `.call()`, `.apply()`, or `.bind()`.
+
+function triva() {
+  console.log(this.a);
+}
+
+var call_triva = {
+  a: 2,
+};
+
+triva.call(call_triva); // 2
+
+// Hard binding;
+function foo() {
+  console.log(this.a);
+}
+
+var obj = { a: 2 };
+
+var bar = function () {
+  foo.call(obj);
+};
+
+bar(); // 2
+setTimeout(bar, 100); // 2
+
+bar.call(window); // 2
+
+// Arguments Pass Garne ra Return Value Receive Garne
+function foo(something) {
+  console.log(this.a, something);
+  return this.a + something;
+}
+
+var obj = { a: 2 };
+
+var bar = function () {
+  return foo.apply(obj, arguments);
+};
+
+var b = bar(3); // 2 3
+console.log(b); // 5
+
+// apply
+function sum(a, b) {
+  return a + b;
+}
+var numbers = [1, 2];
+var result = sum.apply(null, numbers); // 3
+console.log(result);
+
+function bar(something) {
+  console.log(this.a, something);
+  return this.a + something;
+}
+var c = { a: 2 };
+var callBar = bar.bind(c);
+var applyBar = callBar(3);
+console.log("the output will be the 5");
+console.log(applyBar);
--- class/crud.js
+++ class/crud.js
@@ -5,19 +5,50 @@ class AddList {
   }
 
   add(newValue) {
-    const value = (this[this.length] = newValue); // 'new'
-    const value_container = this.store.push(value); // 1;
-    return value, value_container;
+    this.store.push(value);
   }
 
   remove(number) {
-    const remove = value_container.slice(0, number);
-    return remove;
-  } // i have to delete the matching vlaue
+    this.store = this.store.filter((value, index) => {
+      return index !== number;
+    });
+  }
 }
+/* 
+so don't we have the all the thing's what the to do list actually need? 
+we can ad and delte 
+read also. ? 
+update also. okay but we have to acess that value again 
+lik this ..
+arra = [1,2,3,4,5]
+arra[0] = 10; 
+like this..>> ? 
+yes
+so we have to put the index vaue for write that. 
+yes index and new value
+can we check that create and the delet is working or not by using the console.log? or the terminal. 
+can you acess that? 
 
+yes you can check with console for testing
+you did Create and Delete
+but you still need Update and Read
+*/
 const Push = new AddList();
 console.log(Push.add("new"));
+console.log([...push.store]);
+console.log(Push.add("new one"));
+console.log([...push.store]);
+console.log(Push.remove(0));
+console.log([...push.store]);
+
+
+// --- why terminal say that value is not defined?? 
+
+// be right back 
+// okay i will go for dinner then. . 
+// will this works?? 
+// yes
+// push .store for update them? or restore them again? 
 console.log("another one\n\n ----");
 
 // I want to delete the value like this through the index numbers;
@@ -30,5 +61,7 @@ function filterItems(items, idx) {
 
 console.log(filterItems(items, itemIdx));
 
+
+// see this. 
 // if live share is acessed write something's over here. 
 console.log("here is the thing's that i have write it down here. haha");
