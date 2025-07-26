a44f91d8bfa52b7d71e9cbb3e91ed96e049ef79d
bheem-works <shokokomichan098@gmail.com>
2025-07-26 13:42:56 +0545

    push the copy image file

--- copyFeatures/imageCopy/image.js
+++ copyFeatures/imageCopy/image.js
@@ -1,18 +1,33 @@
 // log the error in the paragraph;
 const logElement = document.getElementById('log');
-function log(text){
-    logElement.innerText = `Error: ${text}`; 
+function log(text) {
+    logElement.innerText = `Error: ${text}`;
 }
 
 // reload and clear the examples; 
 const reload = document.getElementById('reload');
-reload.addEventListener('click',()=>{
+reload.addEventListener('click', () => {
     window.location.reload(true);
 })
 
 const destinationImage = document.getElementById('destination');
-destinationImage.addEventListener("click",pasteImage);
+destinationImage.addEventListener("click", pasteImage);
 
-function pasteImage(){
-    
+async function pasteImage() {
+    try {
+        console.log("working");
+        const clipboard = await navigator.clipboard.read();
+
+        for (const item of clipboard) {
+            if (!item.types.includes("image/png")) {
+                console.log('inside the if');
+                throw new Error("clipboard does not contain the png images.");
+            }
+            console.log('out side the if');
+            const blob = await item.getType("image/png");
+            destinationImage.src = URL.createObjectURL(blob);
+        }
+    } catch (error) {
+        log(error.message);
+    }
 }
