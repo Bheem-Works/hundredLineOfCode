939abfbeda6015fd439d48d79533081599b697b2
bheem-works <shokokomichan098@gmail.com>
2025-08-17 13:44:30 +0545

    need to figure it out

--- class/assDes.js
+++ class/assDes.js
@@ -22,3 +22,40 @@ function findAss() {
 
 findAss(); // we just print out the output
 // now remaining is the print;
+
+class AscDsc {
+  // do i need the constructor ? || constructor can be also put by default by the js;
+  /* simply what i want is that 
+    the user can input the value and find out the assecdning or the descending number of it;
+  (number) => find out the ascending;
+     ^^ -----> [1,3,4,2,6,5]
+    user.number([1,3,4,]); 
+    user.showAsscending(); // now it should execute;
+    */
+  number(inputValue) {
+    this.store = [...inputValue];
+    console.log(this.store,"this store");
+    showAsscending(number){
+    for(let i = 0; i < number.length; i++){
+      for(let j = i +1 ; j < number.length; j++ ){
+        let temp;
+        if(number[i] > number[j]){
+          temp = number[i];
+          number[i] = number[j];
+          number[j] = temp;
+        }
+      }
+    }
+  }
+  }
+
+  print() {
+    for(let i = 0; i < this.store.length; i++){
+      console.log(this.store[i]); // this should give the output of the ascending vise; 
+    }
+  }
+}
+
+const show = new AscDsc();
+show.number([1,2,4,5])
+console.log(show.print());
