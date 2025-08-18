0dc7961cf43667772006db98f293a840ba4c94c1
bheem-works <shokokomichan098@gmail.com>
2025-08-18 18:30:27 +0545

    Queue

---
+++ class/Qeue.js
@@ -0,0 +1,6 @@
+// What is queue ?
+// |-> A queue is a type of the data structure that store multiple collection of the items; 
+// -> Add to the back of hte queue (enqueue);
+// -> it also remove the last of the queue(dequeue);
+// -> Referes to the queue to the fifo;
+
