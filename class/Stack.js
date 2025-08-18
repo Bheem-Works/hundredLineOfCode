04dcc8b4f972f0e8ec7ed20702e6d4a022b0f6b3
bheem-works <shokokomichan098@gmail.com>
2025-08-18 18:04:22 +0545

    not got it

--- class/Stack.js
+++ class/Stack.js
@@ -1,47 +1,50 @@
-// Link list by using the class; 
+// Link list by using the class;
 
-class StackNode{
-    constructor(val){
-        this.val = val; 
-        this.next = null; 
-    }
+class StackNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
-class Stack{
-    constructor(){
-        this.top = null;
-        this.size = 0; 
-    }
+class Stack {
+  constructor() {
+    this.top = null;
+    this.size = 0;
+  }
 
-    push(val){
-        if(this.size === 0){
-            this.top = new StackNode(val);
-            console.log('if running');
-        }else{
-            const pushedNode = new StackNode(val);
-            pushedNode.next = this.top;
-            this.top = pushedNode;
-            console.log(this.top,'value should be the more then the one');
-        }
-        this.size++; 
-    }
-    
-    pop(){
-        if(this.size == 0) return null; 
-        const poppedNode = this.top; 
-        this.top = this.top.next;
-        this.size--;
-        return poppedNode.val;
-    }
+  push(val) {
+    if (this.size === 0) {
+      this.top = new StackNode(val);
+    } else {
+      const pushedNode = new StackNode(val);
+      console.log(pushedNode, "pushedNode"); // output : {val:'b',next:null};
 
-    getTop(){
-        return this.top.val;
+      pushedNode.next = this.top;
+      console.log("pushed.next", pushedNode.next); // ouput : Stacknode {val:'a',next : null};
+
+      this.top = pushedNode;
+      console.log(this.top, "this.top"); // ouptu : {val:'b',next : StackNode{val:'a',next:null}}
     }
+    this.size++;
+  }
+
+  pop() {
+    if (this.size == 0) return null;
+    const poppedNode = this.top;
+    console.log("poppedNodeValue", poppedNode); 
+    this.top = this.top.next; // what about the ,next???
+    this.size--;
+    return poppedNode.val;
+  }
+
+  getTop() {
+    return this.top.val;
+  }
 }
 
 const linkList = new Stack();
-linkList.push('a');
-linkList.push('b');
-console.log(linkList.size,'size');
-linkList.pop();
-console.log(linkList.size,'size');
+linkList.push("a");
+linkList.push("b");
+console.log(linkList.pop());
+console.log(linkList.size, "size");
