a32762336234da77f1a4e03da28382d8630ed57e
bheem-works <shokokomichan098@gmail.com>
2025-08-22 09:48:36 +0545

    push

--- class/NestedArray.js
+++ class/NestedArray.js
@@ -1,17 +1,16 @@
-// [1,2,[3,4],5]; => [1,2,3,4,5]; ? 
-// You can also use the flat methods; like this let num = [1,2,[3,4]]; console.log(num.flat()); 
-// And you get the correct output as you expected; 
-// okay lets begin with the real or the venilla methods; 
+// [1,2,[3,4],5]; => [1,2,3,4,5]; ?
+// You can also use the flat methods; like this let num = [1,2,[3,4]]; console.log(num.flat());
+// And you get the correct output as you expected;
+// okay lets begin with the real or the venilla methods;
 
-// We need to do it through the nested loop; 
-let arrNum = [1,2,[3,4],5];
-for(let i = 0; i < arrNum.length; i++){
-    // use this to check if the nested array is the array or not; 
-    if(Array.isArray(arrNum[i])){
-        for(let j = 0; j < arrNum[i].length; j++){
-            console.log(arrNum[i][j]); // Ouput should be like this => [1,2,3,4,5]; 
-        }
-    } else {
-        console.log(arrNum[i]); 
+let arrNum = [1, 2, [3, 4], 5];
+for (let i = 0; i < arrNum.length; i++) {
+  // use this to check if the nested array is the array or not;
+  if (Array.isArray(arrNum[i])) {
+    for (let j = 0; j < arrNum[i].length; j++) {
+      console.log(arrNum[i][j]); // Ouput should be like this => [1,2,3,4,5];
     }
-}; 
+  } else {
+    console.log(arrNum[i]);
+  }
+}
---
+++ jsCodeChallenge/partOne.js
@@ -0,0 +1,4 @@
+// Write a program to find out the extnesion; 
+
+const file = (str) => str.slice(str.lastIndexOf('.'));
+console.log(file("index.css"));
