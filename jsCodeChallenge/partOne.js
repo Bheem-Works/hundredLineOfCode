9a22c270956363a8c7b9734de07aed469e43e24c
bheem-works <shokokomichan098@gmail.com>
2025-08-22 11:12:01 +0545

    push the probelm

--- jsCodeChallenge/partOne.js
+++ jsCodeChallenge/partOne.js
@@ -1,4 +1,16 @@
-// Write a program to find out the extnesion; 
+// Write a program to find out the extnesion;
 
-const file = (str) => str.slice(str.lastIndexOf('.'));
-console.log(file("index.css"));
+const getFileExtension = (str) => str.slice(str.lastIndexOf("."));
+console.log(getFileExtension("index.css"));
+
+// Write a program to replace ever character to the given string with the character following in the ALPHABHET
+const moveStringValue = (str) => {
+  str
+    .split("")
+    .map((char) => String.fromCharCode(char.charCodeAt(0) + 1))
+    .join("");
+};
+
+//
+const findNew = (str) => (str.indexOf("New!") === 0 ? str : "New! " + str);
+console.log(findNew("Hello World"));
