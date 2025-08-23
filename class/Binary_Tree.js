e7a6e0e12b4399a80c3a7daf74cd2a02b4f83470
bheem-works <shokokomichan098@gmail.com>
2025-08-23 23:28:36 +0545

    sound components

--- class/Binary_Tree.js
+++ class/Binary_Tree.js
@@ -1,1 +1,29 @@
-// what is the binary tree ? 
+// what is the binary tree ?
+// -> A binary tree is a tree where the every number has the most two children.
+
+class BinaryNode {
+  constructor(val) {
+    this.val = val;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+const a = new BinaryNode("a");
+const b = new BinaryNode("b");
+const c = new BinaryNode("c");
+const d = new BinaryNode("d");
+const e = new BinaryNode("e");
+const f = new BinaryNode("f");
+
+a.right = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//     a
+//    / \
+//   b   c
+//  / \   \
+// d   e   f
