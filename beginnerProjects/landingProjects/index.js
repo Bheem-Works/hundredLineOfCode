99953719b9e407a5b0b7a7a7be663a79f8eca044
bheem-works <shokokomichan098@gmail.com>
2025-09-08 20:59:51 +0545

    third commit

--- beginnerProjects/landingProjects/index.js
+++ beginnerProjects/landingProjects/index.js
@@ -1,13 +1,20 @@
-const nav = document.createElement('nav');
-const ul = document.createElement('ul');
-const li = document.createElement('li');
-const a = document.createElement('a');
+const nav = document.createElement("nav");
+const ul = document.createElement("ul");
+const li = document.createElement("li");
+const a = document.createElement("a");
 
 /* 
     append the list and to the nav; <nav> <ul> </ul> </nav> 
     append the li to the ul <ul> <li> </li> </ul> 
     append the a to the li <li> <a> </a> </li>
-    
+
 */
 // Style Css:
-nav.style.cssText = 'margin:0px; background:purple; padding:20px';
+// we need to append it to the document or the html;
+nav.style.cssText =
+  "background:purple;display:flex;justify-content:space-between;";
+
+function bodyAppend(input) {
+  document.body.append(input);
+}
+bodyAppend(nav);
