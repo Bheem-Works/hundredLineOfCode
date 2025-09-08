c544ccfa9d69b753df6cb828ffa8def5840e27f9
bheem-works <shokokomichan098@gmail.com>
2025-09-08 21:26:26 +0545

    fourth commit

--- beginnerProjects/landingProjects/index.js
+++ beginnerProjects/landingProjects/index.js
@@ -1,8 +1,18 @@
+/*
+    we do have the 17 elemetns; 
+    make the div for the logo sections; 
+    make the list and the ul anchor tag. 
+    there are 3 buttons and each button have thier logo. 
+*/
 const nav = document.createElement("nav");
+const logo = document.createElement("div");
+const img = document.createElement("img");
 const ul = document.createElement("ul");
 const li = document.createElement("li");
 const a = document.createElement("a");
-
+const home = document.createElement("a");
+const contact = document.createElement("a");
+const menu = document.createElement("a");
 /* 
     append the list and to the nav; <nav> <ul> </ul> </nav> 
     append the li to the ul <ul> <li> </li> </ul> 
@@ -11,10 +21,25 @@ const a = document.createElement("a");
 */
 // Style Css:
 // we need to append it to the document or the html;
-nav.style.cssText =
-  "background:purple;display:flex;justify-content:space-between;";
+nav.append(ul);
+ul.append(li);
+li.append(a);
+function anchor(input) {
+  document.createElement("a").textContent = input;
+  // user can get that value;
+}
+anchor("Why us");
+anchor("Product");
+anchor("Features");
+anchor("Resources");
+anchor("Pricing");
 
+nav.style.cssText =
+  "background:purple;display:flex;justify-content:space-between;margin:0px 0px";
 function bodyAppend(input) {
   document.body.append(input);
 }
 bodyAppend(nav);
+bodyAppend(ul);
+bodyAppend(li);
+bodyAppend(a);
