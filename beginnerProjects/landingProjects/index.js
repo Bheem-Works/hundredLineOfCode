355b22ce50f8377d88991281499efd48b33cee5f
bheem-works <shokokomichan098@gmail.com>
2025-09-08 20:29:15 +0545

    suggestion code

--- beginnerProjects/landingProjects/index.js
+++ beginnerProjects/landingProjects/index.js
@@ -2,5 +2,12 @@ const nav = document.createElement('nav');
 const ul = document.createElement('ul');
 const li = document.createElement('li');
 const a = document.createElement('a');
+
+/* 
+    append the list and to the nav; <nav> <ul> </ul> </nav> 
+    append the li to the ul <ul> <li> </li> </ul> 
+    append the a to the li <li> <a> </a> </li>
+    
+*/
 // Style Css:
 nav.style.cssText = 'margin:0px; background:purple; padding:20px';
