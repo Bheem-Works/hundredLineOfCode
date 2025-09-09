9a8b3afe47f78770715f0743875edf9030ae860b
bheem-works <shokokomichan098@gmail.com>
2025-09-09 11:30:28 +0545

    the loops is not working

--- beginnerProjects/landingProjects/index.js
+++ beginnerProjects/landingProjects/index.js
@@ -9,44 +9,52 @@ const logo = document.createElement("div");
 const img = document.createElement("img");
 const ul = document.createElement("ul");
 const li = document.createElement("li");
-const a = document.createElement("a");
-const home = document.createElement("a");
-const contact = document.createElement("a");
-const menu = document.createElement("a");
+
+// Creating the elements for the anchor;
+const WhyUs = (document.createElement("a").textContent = "Why us");
+const Products = (document.createElement("a").textContent = "Products");
+const Features = (document.createElement("a").textContent = "Features");
+const Resources = (document.createElement("a").textContent = "Resources");
+const Pricing = (document.createElement("a").textContent = "Pricing");
 /* 
     append the list and to the nav; <nav> <ul> </ul> </nav> 
     append the li to the ul <ul> <li> </li> </ul> 
     append the a to the li <li> <a> </a> </li>
-
+ 
 */
-// Style Css:
-// we need to append it to the document or the html;
 nav.append(ul);
 ul.append(li);
-li.append(a);
+li.append(WhyUs);
+li.append(Products);
+li.append(Features);
+li.append(Resources);
+li.append(Pricing);
 
 bodyAppend(nav);
 bodyAppend(ul);
 bodyAppend(li);
-bodyAppend(a);
-//for example;
-const paragraph = document.createElement("li");
-document.body.append(paragraph);
-console.log(paragraph);
-function anchor(input) {
-  //   const inputValue = (document.createElement("a").textContent = input);
-  paragraph.textContent = input;
-//   paragraph.append(input);
-  // user can get that value;
-}
-anchor("Why us");
-anchor("Product");
-anchor("Features");
-anchor("Resources");
-anchor("Pricing");
 
+/* 
+we need to make the list for the each elements; 
+like this
+for(let i = 0; i < 5; i ++){
+const list = document.createElement('li');
+list.append(li);  
+}
+
+auta list ma savai kura haru samayesh vako cha. 
+*/
+
+// Creating the multiple list.
+  for (let i = 0; i < 5; i++) {
+    const newList = document.createElement("li");
+    newList.textContent = "vim";
+  }
+
+nav.append(ul, li);
 nav.style.cssText =
-  "background:purple;display:flex;justify-content:space-between;margin:0px 0px";
+  "background:purple;color:white;margin-top:0px;padding:20px;";
+//for example;
 function bodyAppend(input) {
   document.body.append(input);
 }
