7f201ff48c3b63075be72fee5315e1371d06e404
bheem-works <shokokomichan098@gmail.com>
2025-09-09 12:14:13 +0545

    logo has applied and the image is inserted

--- beginnerProjects/landingProjects/index.js
+++ beginnerProjects/landingProjects/index.js
@@ -3,15 +3,27 @@ const logo = document.createElement('div');
 const img = document.createElement('img');
 const ul = document.createElement('ul');
 const li = document.createElement('li');
-const buttonOne = (document.createElement('button').textContent = 'buttonOne');
-const buttonTwo = (document.createElement('button').textContent = 'buttonTwo');
-const buttonThree = (document.createElement('button').textContent =
-  'buttonThree');
+const buttonOne = document.createElement('button');
+const buttonTwo = document.createElement('button');
+const buttonThree = document.createElement('button');
+
+buttonOne.textContent = 'button One';
+buttonTwo.textContent = 'button two';
+buttonThree.textContent = 'button three';
+
+// let all_button = { buttonOne, buttonTwo, buttonThree };
+// console.log(all_button);
+
 const WhyUs = (document.createElement('a').textContent = 'Why us');
 const Products = (document.createElement('a').textContent = 'Products');
 const Features = (document.createElement('a').textContent = 'Features');
 const Resources = (document.createElement('a').textContent = 'Resources');
 const Pricing = (document.createElement('a').textContent = 'Pricing');
+
+img.setAttribute('src', 'bigWebLogo.png');
+logo.style.cssText = 'display:flex;';
+img.style.cssText = 'width:40px; height:50px;border-radius:50%';
+logo.append(img);
 
 nav.append(ul);
 ul.append(li);
@@ -44,5 +56,5 @@ fourthList.append(Resources);
 fifthList.append(Pricing);
 
 ul.append(firstList, secondList, thirdList, fourthList, fifthList);
-nav.append(ul, buttonOne);
-document.body.append(buttonOne);
+nav.append(logo, ul, buttonOne, buttonTwo, buttonThree);
+// nav.append(ul,all_button);jk
