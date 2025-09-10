7d0299f1a782bea2190c9ce535269d6f4926459c
bheem-works <shokokomichan098@gmail.com>
2025-09-10 20:49:26 +0545

    Button thing's is done

--- beginnerProjects/landingProjects/index.js
+++ beginnerProjects/landingProjects/index.js
@@ -12,9 +12,9 @@ const buttonTwo = document.createElement("button");
 const buttonThree = document.createElement("button");
 const div_button = document.createElement("div");
 const div_anchor = document.createElement("div");
-buttonOne.textContent = "button One";
-buttonTwo.textContent = "button two";
-buttonThree.textContent = "button three";
+buttonOne.textContent = "Call";
+buttonTwo.textContent = "Log In";
+buttonThree.textContent = "Get 14 days Free";
 
 // let all_button = { buttonOne, buttonTwo, buttonThree };
 // console.log(all_button);
@@ -41,8 +41,54 @@ nav.style.cssText =
 img.style.cssText = "width:40px; height:50px;border-radius:50%";
 ul.style.cssText =
   "display:flex;align-items:center;gap:20px; justify-content:center;";
-li.style.cssText = "";
-div_button.style.cssText = "display:flex; gap;20px;";
+li.style.cssText = "list-style:none;style-deo";
+div_button.style.cssText = "display:flex; gap:10px;";
+
+// Css for the list;
+
+// Style for the buttons;
+{
+  buttonOne.style.cssText =
+    "background:transparent; border:2px solid white; border-radius:30%; padding:10px 15px; cursor:pointer;";
+
+  buttonOne.addEventListener("mouseover", () => {
+    buttonOne.style.background = "lightgreen";
+    buttonOne.style.color = "white";
+    buttonOne.style.transform = "scale(1.1)";
+  });
+  buttonOne.addEventListener("mouseout", () => {
+    buttonOne.style.backgroundColor = "transparent";
+    buttonOne.style.color = "";
+    buttonOne.style.transform = "scale(1)";
+  });
+
+  buttonTwo.style.cssText =
+    "background:transparent; border:2px solid white; border-radius:30%; padding:10px 15px; cursor:pointer; ";
+
+  buttonTwo.addEventListener("mouseover", () => {
+    buttonTwo.style.background = "blue";
+    buttonTwo.style.color = "white";
+    buttonTwo.style.transform = "scale(1.1)";
+  });
+  buttonTwo.addEventListener("mouseout", () => {
+    buttonTwo.style.backgroundColor = "transparent";
+    buttonTwo.style.color = "";
+    buttonTwo.style.transform = "scale(1)";
+  });
+  buttonThree.addEventListener("mouseover", () => {
+    buttonThree.style.background = "orange";
+    buttonThree.style.color = "white";
+    buttonThree.style.transform = "scale(1.1)";
+  });
+  buttonThree.addEventListener("mouseout", () => {
+    buttonThree.style.backgroundColor = "transparent";
+    buttonThree.style.color = "";
+    buttonThree.style.transform = "scale(1)";
+  });
+
+  buttonThree.style.cssText =
+    "background:transparent; border:2px solid white; border-radius:30%; padding:10px 15px; cursor:pointer; ";
+}
 //for example;
 function bodyAppend(input) {
   document.body.append(input);
