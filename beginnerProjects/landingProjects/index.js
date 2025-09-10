97eac4cb3174267f069c7a52e622c37e2270ddc1
bheem-works <shokokomichan098@gmail.com>
2025-09-10 21:46:43 +0545

    ohh good why am i stuck

--- beginnerProjects/landingProjects/index.js
+++ beginnerProjects/landingProjects/index.js
@@ -6,12 +6,12 @@ const nav = document.createElement("nav");
 const logo = document.createElement("div");
 const img = document.createElement("img");
 const ul = document.createElement("ul");
-const li = document.createElement("li");
+const div_for_list = document.createElement("div");
+
 const buttonOne = document.createElement("button");
 const buttonTwo = document.createElement("button");
 const buttonThree = document.createElement("button");
 const div_button = document.createElement("div");
-const div_anchor = document.createElement("div");
 buttonOne.textContent = "Call";
 buttonTwo.textContent = "Log In";
 buttonThree.textContent = "Get 14 days Free";
@@ -19,11 +19,43 @@ buttonThree.textContent = "Get 14 days Free";
 // let all_button = { buttonOne, buttonTwo, buttonThree };
 // console.log(all_button);
 
-const WhyUs = (document.createElement("a").textContent = "Why us");
-const Products = (document.createElement("a").textContent = "Products");
-const Features = (document.createElement("a").textContent = "Features");
-const Resources = (document.createElement("a").textContent = "Resources");
-const Pricing = (document.createElement("a").textContent = "Pricing");
+const WhyUs = document.createElement("a");
+WhyUs.textContent = "WhyUs";
+WhyUs.href = "#";
+
+const Products = document.createElement("a");
+Products.textContent = "Products";
+Products.href = "#";
+
+const Features = document.createElement("a");
+Features.textContent = "Features";
+Features.href = "#";
+
+const Resources = document.createElement("a");
+Resources.textContent = "Resources";
+Resources.href = "#";
+
+const Pricing = document.createElement("a");
+Pricing.textContent = "Pricing";
+Pricing.href = "#";
+
+const firstList = document.createElement("li");
+const secondList = document.createElement("li");
+const thirdList = document.createElement("li");
+const fourthList = document.createElement("li");
+const fifthList = document.createElement("li");
+
+[firstList, secondList, thirdList, fourthList, fifthList].forEach((item) => {
+  item.style.cssText =
+    "cursor:pointer; background:black; color:white; padding:5px 10px;";
+});
+
+firstList.append(WhyUs);
+secondList.append(Products);
+thirdList.append(Features);
+fourthList.append(Resources);
+fifthList.append(Pricing);
+ul.append(firstList, secondList, thirdList, fourthList, fifthList);
 
 img.setAttribute("src", "bigWebLogo.png");
 logo.style.cssText = "display:flex;";
@@ -34,17 +66,14 @@ li.append(Features);
 li.append(Resources);
 li.append(Pricing);
 div_button.append(buttonOne, buttonTwo, buttonThree);
-bodyAppend(nav);
 // Css
 nav.style.cssText =
   "background:skyblue;color:white;display:flex; justify-content:space-between;margin-top:-20px;margin-left:-10px;margin-right:-10px;padding:20px;";
 img.style.cssText = "width:40px; height:50px;border-radius:50%";
 ul.style.cssText =
   "display:flex;align-items:center;gap:20px; justify-content:center;";
-li.style.cssText = "list-style:none;style-deo";
+li.style.background = "black";
 div_button.style.cssText = "display:flex; gap:10px;";
-
-// Css for the list;
 
 // Style for the buttons;
 {
@@ -59,7 +88,7 @@ div_button.style.cssText = "display:flex; gap:10px;";
   buttonOne.addEventListener("mouseout", () => {
     buttonOne.style.backgroundColor = "transparent";
     buttonOne.style.color = "";
-    buttonOne.style.transform = "scale(1)";
+    buttonOne.style.transform = "scale(1.1)";
   });
 
   buttonTwo.style.cssText =
@@ -94,17 +123,7 @@ function bodyAppend(input) {
   document.body.append(input);
 }
 
-const firstList = document.createElement("li");
-const secondList = document.createElement("li");
-const thirdList = document.createElement("li");
-const fourthList = document.createElement("li");
-const fifthList = document.createElement("li");
-
-firstList.append(WhyUs);
-secondList.append(Products);
-thirdList.append(Features);
-fourthList.append(Resources);
-fifthList.append(Pricing);
-
-ul.append(firstList, secondList, thirdList, fourthList, fifthList);
+// append it to the nav;
 nav.append(logo, ul, div_button);
+// document.body.append(nav);
+bodyAppend(nav);
